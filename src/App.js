c627d8f9363b799b69f14d6485f6f7c2fc255ebc
Nayak1703 <yashsukantnayak@gmail.com>
2024-11-19 01:17:05 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
-import Checkout from "./components/Checkout"
-import Thanks from "./components/Thanks"
+import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkart-frontend-qlzl.onrender.com/api/v1`,
 };
 
 function App() {
